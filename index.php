dd1c6d71199ec2452a3c53ad5480551312ff4779
Iden <122495928+Idenshah@users.noreply.github.com>
2023-10-20 00:09:46 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Deposit Calculator</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php
+        include('./common/Header.php');
+        ?>
+        <div class="container">
+            <h1>Welcome to Algonquin Bank </h1>
+            <h4>Algonquin Bank is Algonquin College students' most love bank. We provide a set of tools for Algonquin College students to manage their finance.</h4>
+            <div class ="depositCalculator">
+                <ul>
+                    <li>
+                        <a href="Disclaimer.php">Deposit Calculator</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <?php include('./common/Footer.php'); ?>
+    </body>
+</html>
